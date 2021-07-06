71778ce9c6cf3fec82d18bf902e162dbcd00bcf7
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2021-07-06 13:24:19 -0500

    added instruction and skeleton classes for Lab09-ProxyDP

---
+++ Lab09-ProxyDP/src/main/ComplexClass.java
@@ -0,0 +1,13 @@
+package main;
+
+public class ComplexClass {
+
+    public ComplexClass() throws InterruptedException {
+        super();
+        Thread.sleep(10000);
+    }
+
+    public void veryComplicatedTask() {
+        System.out.println("Very complicated task...");
+    }
+}
